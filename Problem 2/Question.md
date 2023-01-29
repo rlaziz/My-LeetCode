8e256858e0eaf71ca7d1438e72969962b271e1b6
rlaziz <rlaziz@bu.edu>
2023-01-29 04:12:06 +0400

    Added Problem 2

--- Problem 2/Question.md
+++ Problem 2/Question.md
@@ -8,7 +8,6 @@ You may assume the two numbers do not contain any leading
 zero, except the number 0 itself.
 
 
-
 ### Constraints
 * The number of nodes in each linked list is in the range `[1,100]`
 * `0 <= Node.val <= 9`
--- Problem 2/Solution.java
+++ Problem 2/Solution.java
@@ -1,14 +1,4 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
+class Solution{
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
         ListNode currentL1 = l1;
